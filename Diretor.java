4c76cd13bacca67539473100b1f9c3c08208b835
Flávio Rodrigues <115443138+Flaviorodrgs@users.noreply.github.com>
2022-10-13 23:38:59 -0300

    Update Diretor.java

--- Diretor.java
+++ Diretor.java
@@ -1,25 +1,4 @@
 package trabalho01;
-
-import java.util.ArrayList;
-
-public class Diretor extends Funcionario implements PessoaFisica, Diretoria {
-
-    // Atributos //
-
-    // Construtor //
-    public Diretor(String nome, String cpf, int conta) {
-        super(cpf, nome, conta);
-        this.setFuncao("Diretor");
-    }
-
-    @Override
-    public String getCPF() {
-        return cpf;
-    }
-
-    @Override
-    public void setCPF(String cpf) {
-        this.cpf = cpf;
-    }
-
+public interface Diretoria {
+    public void infoDiretoria();
 }
