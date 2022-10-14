bad811dd9380d4d82e15af97e3095786d9cf9878
Flávio Rodrigues <115443138+Flaviorodrgs@users.noreply.github.com>
2022-10-13 23:34:59 -0300

    Update SecAcademica.java

--- SecAcademica.java
+++ SecAcademica.java
@@ -2,17 +2,42 @@ package trabalho01;
 
 public class SecAcademica extends Funcionario implements PessoaFisica, Secretaria {
 
+    int h;
+    String Curso;
+
     // Construtor //
     public SecAcademica(String cpf, String nome, int conta) {
         super(cpf, nome, conta);
+        this.setFuncao("Secretaria Academica");
+    }
+    // Getters and Setters//
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
     }
 
     //Métodos//
-    public void ConsultarFrequencia(int faltas) {
-        if (faltas >= 5) {
-            System.out.println("Atingiu o numero maximo de faltas.");
+    public void ConsultarHoras(int horas) {
+        if (horas > 610) {
+            try {
+                throw new ExceptHoras();
+            } catch (ExceptHoras h) {
+                System.out.println("Numero de horas invalidos");
+                h.printStackTrace();
+            }
         } else {
-            System.out.println("O limite de faltas permitidas é 5 faltas");
+            System.out.println("Consultando Horas e Atividades realizadas");
+            System.out.println("...");
+            System.out.println("..");
+            System.out.println(".");
+            h = horas - 610;
+            System.out.println("Qtdade horas necessarias: 610");
+            System.out.println("Qtdade horas obtidas " + horas);
+            System.out.println("Horas a realizar " + this.getH());
         }
     }
 
@@ -25,6 +50,16 @@ public class SecAcademica extends Funcionario implements PessoaFisica, Secretari
     @Override
     public void setCPF(String cpf) {
         this.cpf = cpf;
+    }
+
+    @Override
+    public void infoSecretaria() {
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("CPF: " + this.getCpf());
+        System.out.println("Funcao: " + this.getFuncao());
+        System.out.println("Contas: " + this.getConta());
+        System.out.println("Tipo: Pessoa Fisica");
+
     }
 
 }
