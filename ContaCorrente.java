a54d2374809bf2813a12bb72de1d48f070e98dce
Flávio Rodrigues <115443138+Flaviorodrgs@users.noreply.github.com>
2022-10-13 21:28:07 -0300

    Update ContaCorrente.java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -6,11 +6,12 @@ public class ContaCorrente implements Conta {
     private String titular;
     private float saldo;
     private boolean status;
+    private String gerente;
 
     // Construtor //
-    public ContaCorrente(String titular) {
+    public ContaCorrente(String titular, String gerente) {
         this.titular = titular;
-
+        this.gerente = gerente;
     }
 
     // Getters and Setters //
@@ -30,32 +31,44 @@ public class ContaCorrente implements Conta {
         this.status = status;
     }
 
+    public String getGerente() {
+        return gerente;
+    }
+
+    public void setGerente(String gerente) {
+        this.gerente = gerente;
+    }
+
     // Métodos //
     public void depositar(float v) {
-        if (this.getStatus()) {
+        if (this.getStatus() == false) {
+            try {
+                throw new ExceptConta();
+            } catch (ExceptConta e) {
+                System.out.println("Um erro foi encontrado");
+                e.printStackTrace();
+            }
+        } else {
             this.setSaldo(this.getSaldo() + v);
             System.out.println("Depósito realizado");
-        } else {
-            System.out.println("Não foi possível realizar o deposito");
         }
-
     }
 
-    public void sacar(float v) {
-        if (this.getStatus()) {
-            if (this.getSaldo() >= v) {
-                this.setSaldo(this.getSaldo() - v);
-                System.out.println("Saque realizado");
-            } else {
-                System.out.println("Saldo Insuficiente");
+    public void sacar (float v) {
+        if (this.getStatus() == false) {
+            try {
+                throw new ExceptConta();
+            } catch (ExceptConta e) {
+                System.out.println("Um erro foi encontrado");
+                e.printStackTrace();
             }
-
+        } else {
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Saque realizado");
         }
     }
-
     public void mostrarInfo() {
         System.out.println("Titular: " + this.getTitular());
-        System.out.println("Status: " + this.getStatus());
         System.out.println("Saldo: " + this.getSaldo());
     }
 
