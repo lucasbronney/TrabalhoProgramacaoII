f9230f8fe23fad3aa03e1367b83e401d3cf3daa8
Flávio Rodrigues <115443138+Flaviorodrgs@users.noreply.github.com>
2022-10-13 21:23:43 -0300

    Update SecFinanceira.java

--- SecFinanceira.java
+++ SecFinanceira.java
@@ -1,15 +1,21 @@
 package trabalho01;
 
 public class SecFinanceira extends Funcionario implements PessoaFisica, Secretaria {
-
+   double d = 0.10;
+   double v;
+   double vfinal;
     // Construtor //
     public SecFinanceira(String cpf, String nome, int conta) {
         super(cpf, nome, conta);
+        this.setFuncao("Secretaria Financeira");
     }
-
+    // Getters and Setters //
+    
     // Métodos //
-    public void icsSalario(double v) {
-        this.setSalario(this.getSalario() + v);
+    public void atualizarBoleto(int b) {
+        v = b * d;
+        vfinal = b + v;
+        System.out.println("Valor boleto atualizado: " + vfinal);
     }
 
     @Override
@@ -20,6 +26,15 @@ public class SecFinanceira extends Funcionario implements PessoaFisica, Secretar
     @Override
     public void setCPF(String cpf) {
         this.cpf = cpf;
+    }
+
+    @Override
+    public void infoSecretaria() {
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("CPF: " + this.getCpf());
+        System.out.println("Funcao: " + this.getFuncao());
+        System.out.println("Contas: " + this.getConta());
+        System.out.println("Tipo: Pessoa Fisica");
     }
 
 }
