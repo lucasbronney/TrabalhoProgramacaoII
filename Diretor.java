d4f649b79e8df74efa7a7a2faefe8b5a0792b1d7
Flávio Rodrigues <115443138+Flaviorodrgs@users.noreply.github.com>
2022-10-13 23:39:21 -0300

    Update Diretor.java

--- Diretor.java
+++ Diretor.java
@@ -1,4 +1,54 @@
 package trabalho01;
-public interface Diretoria {
-    public void infoDiretoria();
+
+import java.util.ArrayList;
+
+public class Diretor extends Funcionario implements PessoaFisica, Diretoria {
+
+    // Atributos //
+    public static ArrayList<Disciplina> disciplinas = new ArrayList<>();
+
+    // Construtor //
+    public Diretor(String cpf, String nome, int conta) {
+        super(cpf, nome, conta);
+        this.setFuncao("Diretor");
+    }
+
+    // Getter and Setter //
+    public static ArrayList<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    public static void add(Disciplina d) {
+        disciplinas.add(d);
+        System.out.println("Disciplina adicinada com sucesso. ");
+    }
+    // Métodos //    
+    public static void listarDisciplinas(){
+        System.out.println("Alunos adicionados: \n"+ disciplinas);
+    }
+    
+    public static void rmvDisciplina (Disciplina d){
+        disciplinas.remove(d);
+        System.out.println("Disciplina removida com sucesso.");
+    }
+    
+    @Override
+    public String getCPF() {
+        return cpf;
+    }
+
+    @Override
+    public void setCPF(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public void infoDiretoria() {
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("CPF: " + this.getCpf());
+        System.out.println("Funcao: " + this.getFuncao());
+        System.out.println("Contas: " + this.getConta());
+        System.out.println("Tipo: Pessoa Fisica");
+    }
+
 }
